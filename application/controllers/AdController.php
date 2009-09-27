76d5b6f197b49e34b1938ee34cd2a6ae3af7dcdc
daniel.remeseiro <devnull@localhost>
2009-09-27 21:28:17 +0000

    "removed super light box from change location, and some css fixes"

--- application/controllers/AdController.php
+++ application/controllers/AdController.php
@@ -2,46 +2,46 @@
 
 /**
  * AdController
- * 
+ *
  * @author  dani remeseiro
- * @abstract this is the Ad controller , 
+ * @abstract this is the Ad controller ,
  * do the crud relative to ads : create, show, edit, delete
  */
 
 class AdController extends Zend_Controller_Action {
-	
+
 	/**
 	 * Overriding the init method to also load the session from the registry
 	 *
 	 */
 	public function init() {
 		parent::init ();
-		
+
 		//$this->view->baseUrl = $this->_request->getBaseUrl();
 		$this->view->baseUrl = Zend_Controller_Front::getParam ( $route );
-	
-	
+
+
 	}
-	
+
 	/**
-	 * The default action - show a list where woeid and ad type 
+	 * The default action - show a list where woeid and ad type
 	 * Get the woeid and the ad_type from the session reg
 	 */
 	public function listAction() {
-	    
-	    
+
+
 	    $woeid = $this->_request->getParam ( 'woeid' );
 	    $ad_type = $this->_request->getParam ( 'ad_type' );
-        
+
 		$model = $this->_getModel ();
 		$this->view->ad = $model->getAdList($woeid, $ad_type);
-		
-		
+
+
 		//set the location reg var from the url
 		$aNamespace = new Zend_Session_Namespace('Nolotiro');
         $aNamespace->location = $woeid;
-        
-        
+
+
         //paginator
         $page = $this->_getParam('page');
         $paginator = Zend_Paginator::factory($this->view->ad);
@@ -51,164 +51,164 @@ class AdController extends Zend_Controller_Action {
 
         $this->view->paginator=$paginator;
 
-        
-		
+
+
 		///
 		$this->_flashMessenger = $this->_helper->getHelper ( 'FlashMessenger' );
 		$this->view->mensajes = $this->_flashMessenger->getMessages ();
-		
 
-		
+
+
 	}
-	
+
 	public function showAction() {
-		
-		
+
+
 		$id = $this->_request->getParam ( 'id' );
-		
+
 		$model = $this->_getModel ();
-		
+
 		$this->view->ad = $model->getAd( $id );
 	    $this->view->comments = $model->getComments( $id );
-		
-		
-		
+
+
+
 	}
-	
+
 	public function createAction() {
-		
+
 	    $locale = Zend_Registry::get ( "Zend_Locale" );
         $lang = $locale->getLanguage ();
-	    
+
 		//first we check if user is logged, if not redir to login
 		$auth = Zend_Auth::getInstance ();
 		if (! $auth->hasIdentity ()) {
 			$this->_redirect ( $lang.'/auth/login' );
-			
-			
+
+
 		} else {
-			
+
 			$request = $this->getRequest ();
 			$form = $this->_getAdEditForm ();
-			
+
 			// check to see if this action has been POST'ed to
 			if ($this->getRequest ()->isPost ()) {
-				
+
 				// now check to see if the form submitted exists, and
 				// if the values passed in are valid for this form
 				if ($form->isValid ( $request->getPost () )) {
-					
+
 					$formulario = $form->getValues ();
-					
+
 					//strip html tags to title
 					$formulario['title'] = strip_tags($formulario['title']);
-					
+
 					//anti hoygan to title
 					//dont use strtolower because dont convert utf8 properly . ej: á é ó ...
-					$formulario['title'] = ucfirst(mb_convert_case($formulario['title'], MB_CASE_LOWER, "UTF-8")); 
-					
+					$formulario['title'] = ucfirst(mb_convert_case($formulario['title'], MB_CASE_LOWER, "UTF-8"));
+
 					//strip html tags to body
 					$formulario['body'] = strip_tags($formulario['body']);
-					
+
 					//anti hoygan to body
 					$split=explode(". ", $formulario['body']);
-                    
+
 					foreach ($split as $sentence) {
                         $sentencegood = ucfirst(mb_convert_case($sentence, MB_CASE_LOWER, "UTF-8"));
                         $formulario['body'] = str_replace($sentence, $sentencegood, $formulario['body']);
                     }
-					
-                    
+
+
                     //get the ip of the ad publisher
-                    if (getenv(HTTP_X_FORWARDED_FOR)) {							
-                        $ip = getenv(HTTP_X_FORWARDED_FOR); 
-                    } else { 
+                    if (getenv(HTTP_X_FORWARDED_FOR)) {
+                        $ip = getenv(HTTP_X_FORWARDED_FOR);
+                    } else {
                         $ip = getenv(REMOTE_ADDR);
                     }
-            
-                    $formulario['ip'] = $ip;        
-                    
+
+                    $formulario['ip'] = $ip;
+
 					//get this ad user owner
 					$formulario ['user_owner'] = $auth->getIdentity ()->id;
-					
+
 					//get date created
                     //TODO to use the Zend Date object to apapt the time to the locale user zone
 					$datenow = date("Y-m-d H:i:s", time() );
 					$formulario ['date_created'] = $datenow;
-					
+
 					//get woeid to assign to this ad
-					//the location its stored at session location value 
+					//the location its stored at session location value
                     //(setted by default on bootstrap to Madrid woeid number)
                     $aNamespace = new Zend_Session_Namespace('Nolotiro');
                     $formulario ['woeid_code'] = $aNamespace->location;
-					
-					
+
+
 					$model = $this->_getModel ();
 					$model->save ( $formulario );
-					
+
 					//Zend_Debug::dump ( $formulario );
                     $this->_helper->_flashMessenger->addMessage ( $this->view->translate ( 'Ad published succesfully!' ) );
-					$this->_redirect ( '/' );
-					
+					$this->_redirect ( '/'.$lang.'/ad/list/woeid/'.$aNamespace->location.'/ad_type/give' );
+
 				}
 			}
 		}
-	
+
 	}
-	
+
 	public function editAction() {
 		$request = $this->getRequest ();
 		$form = $this->_getAdEditForm ();
-		
+
 		// check to see if this action has been POST'ed to
 		if ($this->getRequest ()->isPost ()) {
-			
+
 			// now check to see if the form submitted exists, and
 			// if the values passed in are valid for this form
 			if ($form->isValid ( $request->getPost () )) {
-				
+
 				// since we now know the form validated, we can now
 				// start integrating that data submitted via the form
 				// into our model
 				$formulario = $form->getValues ();
 				Zend_Debug::dump ( $formulario );
-			
+
 			}
 		}
 	}
-	
+
 	/**
 	 *
-	 * @return Form_AdEdit 
+	 * @return Form_AdEdit
 	 */
 	protected function _getAdEditForm() {
 		require_once APPLICATION_PATH . '/forms/AdEdit.php';
 		$form = new Form_AdEdit ( );
-		
+
 		// assign the form to the view
 		$this->view->form = $form;
 		return $form;
 	}
-	
+
 	public function deleteAction() {
-	
+
 	}
-	
+
 	/**
 	 * _getModel() is a protected utility method for this controller.
-	 * 
+	 *
 	 * @return Model_User
 	 */
 	protected function _getModel() {
 		if (null === $this->_model) {
-			
+
 			require_once APPLICATION_PATH . '/models/Ad.php';
 			$this->_model = new Model_Ad ( );
 		}
 		return $this->_model;
 	}
-	
-	
+
+
 
 }
 
--- application/controllers/LocationController.php
+++ application/controllers/LocationController.php
@@ -1,157 +1,161 @@
 <?php
 /**
  * LocationController
- * 
+ *
  */
 
 class LocationController extends Zend_Controller_Action {
-	
+
 	public function init() {
 		parent::init ();
 		$this->view->baseUrl = Zend_Controller_Front::getParam ( $route );
-		
-	}
-	
-	
-	/**
-	 * get the location stored at session location value 
-	 * (setted by default on bootstrap to Madrid woeid number)
-	 * 
-	 */
-	
-	public function indexAction(){
-		//$this->_redirect ( '/' );
-		
-		 
+
 	}
 
-	
-	
-	
+
+	/**
+	 * get the location stored at session location value
+	 * (setted by default on bootstrap to Madrid woeid number)
+	 *
+	 */
+
+	public function indexAction(){
+		//$this->_redirect ( '/' );
+
+
+	}
+
+
+
+
 	public function changeAction(){
 		$request = $this->getRequest();
 		$form = $this->_getLocationChangeForm();
-		
-		
+
+
 		// check to see if this action has been POST'ed to
 		if ($this->getRequest ()->isPost ()) {
-			
-			
+
+
 			if ($form->isValid ( $request->getPost () )) {
-				
-				
+
+
 				$formulario = $form->getValues ();
-				
+
 				$aNamespace = new Zend_Session_Namespace('Nolotiro');
 				$aNamespace->locationTemp = $formulario['location'];
-				
+
 				$locale = Zend_Registry::get ( "Zend_Locale" );
                 $this->lang = $locale->getLanguage ();
-                
+
 				$this->_redirect ( '/'.$this->lang.'/location/change2' );
-			
+
 			}
 		}
 		// assign the form to the view
 		$this->view->form = $form;
-		
-		
+
+
 	}
-	
+
 	public function change2Action(){
-	    
+
 	    $aNamespace = new Zend_Session_Namespace('Nolotiro');
 		$locationtemp = $aNamespace->locationTemp;
-				
+
 		$places = $this->getYahooGeoWoeidList($locationtemp);
-		
+
 		$request = $this->getRequest();
 		$form = $this->_getLocationChange2Form($locationtemp);
-		
-		
+
+
 		// assign the form to the view
 		$this->view->locationtemp = $locationtemp;
 		$this->view->places = $places;
-		
+
 		$this->view->form = $form;
-		
+
 		//*** here add the select values to the form from yahoo xml result
-		
+
 		foreach ($places as $item) {
-       
-    		$name = $item->name.', '.$item->admin2.', '.$item->country;    
-    		
+
+    		$name = $item->name.' ('.$item->placeTypeName.'), '.$item->admin2.', '.$item->country;
+
     		$woeid = (string)$item->woeid; //we have to set to string to not disturb the zend form translate parser!
-            
+
     		//glue together woeid and text to parse after with *
     		$woeid = $woeid.'*'.$name;
-    		
+
             $location_options[$woeid]= $name;
-        
+
         }
-		
-        
-        
+
+
+
 		$form->addElement('select', 'location', array('validators'))
         ->getElement('location')
         ->addMultiOptions($location_options)
         ->setIsArray(true);//this set select expanded
-        
-		
+
+
 		// add the submit button
 		$form->addElement ( 'submit', 'submit', array ('label' => 'Choose your location' ) );
-		
-		
+
+
 		// check to see if this action has been POST'ed to
 		if ($this->getRequest ()->isPost ()) {
-			
-			
+
+
 			if ($form->isValid ( $request->getPost () )) {
-				
-				
+
+
 				$formulario = $form->getValues ();
-				
+
 				//parse the location value
 				$values = explode("*", $formulario['location'][0]);
-                
-				
+
+
 				$aNamespace = new Zend_Session_Namespace('Nolotiro');
 				$aNamespace->location = $values[0];//woeid
-				
-		        $name = $item->name.', '.$item->admin2.', '.$item->country;
-		    
-		        
-				
+
+		        $name = $item->name.', '.$item->admin1.', '.$item->country;
+
+
+
 				$aNamespace->locationName = $values[1];//location name
-				
-				
-				$this->_helper->_flashMessenger->addMessage ( $this->view->translate ( 'Location changed successfully!' ) );
-				
-				//$this->_redirect ( '/' );
-			
+
+
+				$this->_helper->_flashMessenger->addMessage ( $this->view->translate ( 'Location changed successfully to ' )
+				.$values[1]);
+
+				$locale = Zend_Registry::get ( "Zend_Locale" );
+                $lang = $locale->getLanguage ();
+
+				$this->_redirect ( '/'.$lang.'/ad/list/woeid/'.$values[0].'/ad_type/give' );
+
 			}
 		}
-		
-		
-		
+
+
+
 	}
-	
+
 	public function getYahooGeoWoeidList($locationtemp){
-	    
+
 	    //get the user session language to fetch the proper yahoo xml language
 	    $locale = Zend_Registry::get ( "Zend_Locale" );
-        $lang = $locale->getLanguage (); 
-	    
+        $lang = $locale->getLanguage ();
+
         $appid = ('bqqsQazIkY0X4bnv8F9By.m8ZpodvOu6');
-	    $htmlString = 'http://where.yahooapis.com/v1/places.q(%27'.$locationtemp.'%27);start=0;count=10'.
+	    $htmlString = 'http://where.yahooapis.com/v1/places.q('.urlencode($locationtemp).');start=0;count=10'.
 	    '?appid='.$appid.'&lang='.$lang;
-	    
+
 	    $xml = simplexml_load_file($htmlString);
-	    
+
 	    return $xml;
-	    
+
 	}
-	
+
 	/**
 	 *
 	 * @return Form_LocationChange
@@ -162,14 +166,14 @@ class LocationController extends Zend_Controller_Action {
 		//$form->setAction($this->_helper->url(''));
 		return $form;
 	}
-	
+
 	protected function _getLocationChange2Form($locationtemp) {
 		require_once APPLICATION_PATH . '/forms/LocationChange2.php';
 		$form = new Form_LocationChange2();
 		//$form->setAction($this->_helper->url(''));
 		return $form;
 	}
-	
+
 }
 
 
