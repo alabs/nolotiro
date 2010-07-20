d1f0c8d3ea36bad2cab8d3d94a3cb9678101e6d2
daniel.remeseiro <devnull@localhost>
2010-07-20 17:25:43 +0200

    fixed bug, somtiemes lost the value of location name, due to bad conversion json encode- decode to serialization, now using out own serialize -unserialize funcionts after save and load from memcached

--- application/controllers/LocationController.php
+++ application/controllers/LocationController.php
@@ -1,252 +1,235 @@
 <?php
+
 /**
  * @author Dani Remeseiro
  * @license http://www.gnu.org/licenses/agpl-3.0.html
  *
  */
-
 class LocationController extends Zend_Controller_Action {
 
-	public function init() {
+    public function init() {
 
-             $this->lang = $this->view->lang =  $this->_helper->checklang->check();
-             $this->view->checkMessages  = $this->_helper->checkMessages->check();
-                
-	}
+        $this->lang = $this->view->lang = $this->_helper->checklang->check();
+        $this->view->checkMessages = $this->_helper->checkMessages->check();
+    }
+
+    public function indexAction() {
+        $this->_redirect('/');
+    }
+
+    public function changeAction() {
+        $request = $this->getRequest();
+        $form = $this->_getLocationChangeForm();
+
+        if ($this->getRequest()->isPost()) {
 
 
-        public function indexAction(){
-             $this->_redirect ( '/' );
+            if ($form->isValid($request->getPost())) {
+
+                $formulario = $form->getValues();
+
+                //convert to lowercase and clean spaces
+                $formulario['location'] = ucfirst(mb_convert_case(trim($formulario['location']), MB_CASE_LOWER, "UTF-8"));
+
+                $aNamespace = new Zend_Session_Namespace('Nolotiro');
+                $aNamespace->locationTemp = $formulario['location'];
+
+                $this->_redirect('/' . $this->view->lang . '/location/change2');
+            }
+        }
+        // assign the form to the view
+        $this->view->form = $form;
+    }
+
+    public function change2Action() {
+
+        $aNamespace = new Zend_Session_Namespace('Nolotiro');
+        $locationtemp = $aNamespace->locationTemp;
+
+
+        $town = $this->view->translate('Town');
+        $places = $this->getYahooGeoWoeidList($locationtemp, $this->view->lang, $town);
+
+
+        //check if we got response from yahoo geo api
+        if ($places === false) {
+            $this->_helper->_flashMessenger->addMessage(
+                    $this->view->translate('I can not connect to Yahoo geo service, sorry!'));
+            $this->_redirect('/' . $this->view->lang . '/woeid/' . $aNamespace->location . '/give');
         }
 
+        //check if the yahoo geo api returns no results!
+        if (count($places->place) == 0) {
 
-	public function changeAction(){
-		$request = $this->getRequest();
-		$form = $this->_getLocationChangeForm();
-		
-		if ($this->getRequest ()->isPost ()) {
+            $this->_helper->_flashMessenger->addMessage(
+                    $this->view->translate('No location found named:') . '  "' . $locationtemp . '"');
+            $this->_redirect('/' . $this->view->lang . '/woeid/' . $aNamespace->location . '/give');
+        }
 
-
-			if ($form->isValid ( $request->getPost () )) {
-
-				$formulario = $form->getValues ();
-
-                                //convert to lowercase and clean spaces
-                                $formulario['location'] = ucfirst(mb_convert_case(trim($formulario['location']), MB_CASE_LOWER, "UTF-8"));
-
-				$aNamespace = new Zend_Session_Namespace('Nolotiro');
-				$aNamespace->locationTemp = $formulario['location'];
-
-				$this->_redirect ( '/'.$this->view->lang.'/location/change2' );
-				
-			}
-
-                }
-		// assign the form to the view
-		$this->view->form = $form;
-
-	}
-
-	public function change2Action(){
-
-		$aNamespace = new Zend_Session_Namespace('Nolotiro');
-		$locationtemp = $aNamespace->locationTemp;
-
-                
-		$town =$this->view->translate('Town');
-		$places = $this->getYahooGeoWoeidList($locationtemp, $this->view->lang, $town);
-
-		//var_dump($places);
-                
-		//check if we got response from yahoo geo api
-		if ($places === false) {
-			$this->_helper->_flashMessenger->addMessage (
-				$this->view->translate ( 'I can not connect to Yahoo geo service, sorry!'));
-                                $this->_redirect ( '/'.$this->view->lang.'/woeid/'.$aNamespace->location.'/give' );
-			
-		}
-
-		//check if the yahoo geo api returns no results!
-		if ( count($places->place ) == 0){
-					
-			$this->_helper->_flashMessenger->addMessage (
-				$this->view->translate ( 'No location found named:') .'  "'. $locationtemp .'"');
-                                $this->_redirect ( '/'.$this->view->lang.'/woeid/'.$aNamespace->location.'/give' );
-		
-		}
-
-		$request = $this->getRequest();
-		$form = $this->_getLocationChange2Form($locationtemp);
+        $request = $this->getRequest();
+        $form = $this->_getLocationChange2Form($locationtemp);
 
 
-		// assign the form to the view
-		$this->view->locationtemp = $locationtemp;
-		$this->view->places = $places;
+        // assign the form to the view
+        $this->view->locationtemp = $locationtemp;
+        $this->view->places = $places;
 
-		$this->view->form = $form;
+        $this->view->form = $form;
 
-		//*** here add the select values to the form from yahoo xml result
-                
-		foreach ($places->place as $item) {
+        //*** here add the select values to the form from yahoo xml result
+        foreach ($places->place as $item) {
+            $name = $item->name . ', ' . $item->admin1 . ', ' . $item->country;
+            $woeid = (string) $item->woeid; //we have to cast to string item to not disturb the zend form translate parser!
+            //glue together woeid and text to parse after with *
+            $woeid = $woeid . '*' . $name;
 
-			
-                        $name = $item->name.', '.$item->admin1.', '.$item->country;
-			$woeid = (string)$item->woeid; //we have to cast to string item to not disturb the zend form translate parser!
-	
-			//glue together woeid and text to parse after with *
-			$woeid = $woeid.'*'.$name;
-	
-			$location_options[$woeid]= $name;
+            $location_options[$woeid] = $name;
 
-			//check the first value of the array results to show the first selected to form
-			$counter++;
-			if ($counter == 1) {
-			    $firstitem = $woeid;
-			}
+            //check the first value of the array results to show the first selected to form
+            $counter++;
+            if ($counter == 1) {
+                $firstitem = $woeid;
+            }
+        }
 
-		}
-
-
-
-		$form->addElement('select', 'location', array('validators'))
-		->getElement('location')
-		->addMultiOptions($location_options)
-		->setValue($firstitem)
+        $form->addElement('select', 'location', array('validators'))
+                ->getElement('location')
+                ->addMultiOptions($location_options)
+                ->setValue($firstitem)
                 ->setRegisterInArrayValidator(false)
-		->setIsArray(true);//this set select expanded
+                ->setIsArray(true); //this set select expanded
+        // add the submit button
+        $form->addElement('submit', 'submit', array('label' => 'Choose your location'));
 
 
-		// add the submit button
-		$form->addElement ( 'submit', 'submit', array ('label' => 'Choose your location' ) );
+        // check to see if this action has been POST'ed to
+        if ($this->getRequest()->isPost()) {
+
+            if ($form->isValid($request->getPost())) {
+
+                $formulario = $form->getValues();
+
+                //parse the location value
+                $values = explode("*", $formulario['location'][0]);
+
+                //if the user is logged then update the woeid value, if not just update the session location value
+                $auth = Zend_Auth::getInstance ();
+                if ($auth->hasIdentity()) {
+
+                    require_once APPLICATION_PATH . '/models/User.php';
+                    $model = new Model_User();
+                    $data['id'] = $auth->getIdentity()->id;
+                    $data['woeid'] = $values[0];
+                    $userUpdateLocation = $model->update($data);
+                }
+
+                $aNamespace = new Zend_Session_Namespace('Nolotiro');
+                $aNamespace->location = $values[0]; //woeid
+                setcookie('location', $values[0], null, '/');
+
+                $name = $item->name . ', ' . $item->admin1 . ', ' . $item->country;
+
+                $aNamespace->locationName = $values[1]; //location name
+
+                $this->_helper->_flashMessenger->addMessage($this->view->translate('Location changed successfully to:')
+                        . ' ' . $values[1]);
 
 
-		// check to see if this action has been POST'ed to
-		if ($this->getRequest ()->isPost ()) {
+                $this->_redirect('/' . $this->view->lang . '/woeid/' . $values[0] . '/give');
+            }
+        }
+    }
 
+    public function getYahooGeoWoeidList($locationtemp, $lang, $town) {
 
-			if ($form->isValid ( $request->getPost () )) {
-
-
-				$formulario = $form->getValues ();
-
-				//parse the location value
-				$values = explode("*", $formulario['location'][0]);
-
-
-                                //if the user is logged then update the woeid value, if not just update the session location value
-                                $auth = Zend_Auth::getInstance ();
-                                if ($auth->hasIdentity ()) {
-
-                                     require_once APPLICATION_PATH . '/models/User.php';
-                                    $model = new Model_User();
-                                    $data['id'] = $auth->getIdentity()->id;
-                                    $data['woeid'] = $values[0];
-                                    $userUpdateLocation = $model->update($data);
-
-                                }
-
-				$aNamespace = new Zend_Session_Namespace('Nolotiro');
-				$aNamespace->location = $values[0];//woeid
-                                setcookie ( 'location', $values[0], null, '/' );
-
-				$name = $item->name.', '.$item->admin1.', '.$item->country;
-
-				$aNamespace->locationName = $values[1];//location name
-
-				$this->_helper->_flashMessenger->addMessage ( $this->view->translate ( 'Location changed successfully to:' )
-				.' '.$values[1]);
-
-                                
-				$this->_redirect ( '/'.$this->view->lang.'/woeid/'.$values[0].'/give' );
-
-			}
-		}
-
-
-
-	}
-
-	public function getYahooGeoWoeidList($locationtemp,$lang,$town){
-
-             //lets use memcached to not waste yahoo geo api requests
-            // configure caching backend strategy
-            $oBackend = new Zend_Cache_Backend_Memcached(
-                    array(
-                            'servers' => array( array(
+        //lets use memcached to not waste yahoo geo api requests
+        // configure caching backend strategy
+        $oBackend = new Zend_Cache_Backend_Memcached(
+                        array(
+                            'servers' => array(array(
                                     'host' => '127.0.0.1',
                                     'port' => '11211'
-                            ) ),
+                            )),
                             'compression' => true
-            ) );
+                ));
 
-            // configure caching frontend strategy
-            $oFrontend = new Zend_Cache_Core(
-                    array(
+        // configure caching frontend strategy
+        $oFrontend = new Zend_Cache_Core(
+                        array(
                             'caching' => true,
                             'cache_id_prefix' => 'woeidList',
                             'logging' => FALSE,
                             'write_control' => true,
-                            'automatic_serialization' =>true,
+                            'automatic_serialization' => true,
                             'ignore_user_abort' => true
-                    ) );
+                ));
 
-            // build a caching object
-            $cache = Zend_Cache::factory( $oFrontend, $oBackend );
+        // build a caching object
+        $cache = Zend_Cache::factory($oFrontend, $oBackend);
 
-            //locationtemp normalize spaces and characters not allowed (ñ) by memcached to create the item name
-            $locationtempHash = md5($locationtemp );
+        //locationtemp normalize spaces and characters not allowed (ñ) by memcached to create the item name
+        $locationtempHash = md5($locationtemp);
 
-            $cachetest = $cache->test($locationtempHash.$lang);
+        $cachetest = $cache->test('Loc' . $locationtempHash . $lang);
 
-            if ( $cachetest == false ){
+        if ($cachetest == false) {
 
-                $appid = ('bqqsQazIkY0X4bnv8F9By.m8ZpodvOu6');
-		$htmlString = "http://where.yahooapis.com/v1/places\$and(.q(".
-                urlencode($locationtemp)."),.type(".$town."));count=30?appid=".$appid."&lang=".$lang;
+            $appid = ('bqqsQazIkY0X4bnv8F9By.m8ZpodvOu6');
+            $htmlString = "http://where.yahooapis.com/v1/places\$and(.q(" .
+                    urlencode($locationtemp) . "),.type(" . $town . "));count=30?appid=" . $appid . "&lang=" . $lang;
 
-		$xml = simplexml_load_file($htmlString);
+            $xml = simplexml_load_file($htmlString);
 
-                // due to simplexml is unable to put xml into memcached, we have to convert to objects
-                // the json_decode(json_encode...  converts all the SimpleXML elements into stdClass objects
-                
-                $cache->save(json_decode(json_encode($xml)), $locationtempHash.$lang);
-                
-                //var_dump('no cached!!');
-                } else {
-                 
-                $xml = $cache->load($locationtempHash.$lang);
-                //var_dump('***********cached!!');
+            // due to simplexml is unable to put xml into memcached, we have to convert to objects
+            // to stdclass with our special functions serialize and unserialize
+            $cache->save($this->_serializemmp($xml), 'Loc' . $locationtempHash . $lang);
+            //var_dump('no cached!!');
+        } else {
 
-                }
-            
-		return $xml;
+            $xml = $this->_unserializemmp($cache->load('Loc' . $locationtempHash . $lang));
+            //var_dump('***********cached!!');
+        }
 
-	}
+        return (object) $xml;
+    }
 
+    //*************************************************************
+    protected  function _serializemmp($toserialize) {
+        if (is_a($toserialize, "SimpleXMLElement")) {
+            $stdClass = new stdClass();
+            $stdClass->type = get_class($toserialize);
+            $stdClass->data = $toserialize->asXml();
+        }
+        return serialize($stdClass);
+    }
 
-        
+    protected  function _unserializemmp($tounserialize) {
+        $tounserialize = unserialize($tounserialize);
+        if (is_a($tounserialize, "stdClass")) {
+            if ($tounserialize->type == "SimpleXMLElement") {
+                $tounserialize = simplexml_load_string($tounserialize->data);
+            }
+        }
+        return $tounserialize;
+    }
 
+    /**
+     *
+     * @return Form_LocationChange
+     */
+    protected function _getLocationChangeForm() {
+        require_once APPLICATION_PATH . '/forms/LocationChange.php';
+        $form = new Form_LocationChange();
+        //$form->setAction($this->_helper->url(''));
+        return $form;
+    }
 
-	/**
-	 *
-	 * @return Form_LocationChange
-	 */
-	protected function _getLocationChangeForm() {
-		require_once APPLICATION_PATH . '/forms/LocationChange.php';
-		$form = new Form_LocationChange();
-		//$form->setAction($this->_helper->url(''));
-		return $form;
-	}
-
-	protected function _getLocationChange2Form($locationtemp) {
-		require_once APPLICATION_PATH . '/forms/LocationChange2.php';
-		$form = new Form_LocationChange2();
-		//$form->setAction($this->_helper->url(''));
-		return $form;
-	}
-
-
+    protected function _getLocationChange2Form($locationtemp) {
+        require_once APPLICATION_PATH . '/forms/LocationChange2.php';
+        $form = new Form_LocationChange2();
+        //$form->setAction($this->_helper->url(''));
+        return $form;
+    }
 
 }
-
 
