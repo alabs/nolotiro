36c72877509d147170c122250afc02e612ef7dc5
daniel.remeseiro <devnull@localhost>
2010-11-05 20:31:00 +0100

    doing query check locked user helper just if user have session

--- application/controllers/helpers/CheckLockedUser.php
+++ application/controllers/helpers/CheckLockedUser.php
@@ -6,13 +6,17 @@ class Zend_Controller_Action_Helper_CheckLockedUser extends Zend_Controller_Acti
          function check(){
 
              $auth = Zend_Auth::getInstance ();
-             $id = $auth->getIdentity()->id;
+
+             if ($auth->hasIdentity()){
+
+               //get user id from auth object
+              $id = $auth->getIdentity()->id;
 
               //search on ddbb user locked value
               require_once APPLICATION_PATH . '/models/User.php';
               $model = new Model_User();
               $locked =  $model->CheckLockedUser( (int)$id) ;
-
+             }
 
               return $locked;
 
