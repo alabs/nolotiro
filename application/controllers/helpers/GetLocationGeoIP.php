173912a7048ea1dc7a5cead4ed1a50d24318ad91
Daniel Remeseiro <daniel.remeseiro@gmail.com>
2012-04-04 15:55:03 +0200

    added translations to fr, gl, cat, and some css fixes

--- application/controllers/helpers/GetLocationGeoIP.php
+++ application/controllers/helpers/GetLocationGeoIP.php
@@ -1,41 +1,39 @@
 <?php
 /**
- * Description of Woeid
- *
  * @abstract this helper uses the GeoIP Api
  * @author dani remeseiro
  */
-class Zend_Controller_Action_Helper_GetLocationGeoIP extends Zend_Controller_Action_Helper_Abstract {
+class Zend_Controller_Action_Helper_GetLocationGeoIP extends Zend_Controller_Action_Helper_Abstract
+{
 
 
-    public  function suggest(){
+    public function suggest()
+    {
+        require_once (NOLOTIRO_PATH . '/library/GeoIP/geoipcity.inc');
 
-		require_once ( NOLOTIRO_PATH . '/library/GeoIP/geoipcity.inc' );
-
-                if (getenv(HTTP_X_FORWARDED_FOR)) {
-                    $ip = getenv(HTTP_X_FORWARDED_FOR);
-                } else {
-                    $ip = getenv(REMOTE_ADDR);
-                }
-
-                
-                //$ip = '67.195.114.53';//yahoo slurp
-                //$ip = '66.249.71.206';//google bot
+        if (getenv(HTTP_X_FORWARDED_FOR)) {
+            $ip = getenv(HTTP_X_FORWARDED_FOR);
+        } else {
+            $ip = getenv(REMOTE_ADDR);
+        }
 
 
-		$gi = geoip_open("/usr/local/share/GeoIP/GeoLiteCity.dat",GEOIP_STANDARD);
-	        $record = geoip_record_by_addr($gi,$ip);
-	        
-	        //$result .= $record->region . " " . $GEOIP_REGION_NAME[$record->country_code][$record->region] . "\n";
-	        $result = $record->city.', ' ;
-                $result .= $GEOIP_REGION_NAME[$record->country_code][$record->region].', ';
-                $result .= $record->country_name;
+        //this ips are for dev env, to show something (localhost ip doesnt work)
+        //$ip = '67.195.114.53';//yahoo slurp
+        //$ip = '66.249.71.206';//google bot
 
-		
-		geoip_close($gi);
 
-                return $result;
+        $gi = geoip_open("/usr/local/share/GeoIP/GeoLiteCity.dat", GEOIP_STANDARD);
+        $record = geoip_record_by_addr($gi, $ip);
 
-	}
+        //$result .= $record->region . " " . $GEOIP_REGION_NAME[$record->country_code][$record->region] . "\n";
+        $result = $record->city . ', ';
+        $result .= $GEOIP_REGION_NAME[$record->country_code][$record->region] . ', ';
+        $result .= $record->country_name;
+
+        geoip_close($gi);
+
+        return $result;
+    }
 
 }
