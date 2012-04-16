5ce6aa266b7a38f1ad54dab1a65249ac94a39c2d
Daniel Remeseiro <daniel.remeseiro@gmail.com>
2012-04-16 19:56:33 +0200

    changed ad url routes on rss feeds

--- application/controllers/RssController.php
+++ application/controllers/RssController.php
@@ -3,18 +3,14 @@
 class RssController extends Zend_Controller_Action {
 
     public function init() {
-
         $this->lang = $this->view->lang = $this->_helper->checklang->check();
-
     }
 
 
 
     public function preDispatch()  {
-
         $this->_helper->viewRenderer->setNoRender();
         $this->_helper->layout()->disableLayout();
-
     }
 
 
@@ -33,22 +29,17 @@ class RssController extends Zend_Controller_Action {
         $rss['description'] = 'nolotiro.org - '. $this->_helper->woeid->name($woeid, $this->lang);
         $rss['language'] = $this->lang;
         $rss['generator'] = 'nolotiro.org';
-
         $rss['entries'] = array();
-
-
 
 
         foreach ($this->ads as $value) {
 
             $entry = array();
             $entry['title'] = $value['title'];
-            $entry['link'] = 'http://' . $_SERVER['HTTP_HOST'] .'/'.$this->lang.'/ad/show/id/'.$value['id'].'/'.$this->view->slugTitle($value['title']);
+            $entry['link'] = 'http://' . $_SERVER['HTTP_HOST'] .'/'.$this->lang.'/ad/'.$value['id'].'/'.$this->view->slugTitle($value['title']);
             $entry['description'] = $this->view->escapeEmail($value['body']);
             $entry['lastUpdate'] = strtotime($value['date_created']);
-
             $rss['entries'][]  = $entry;
-
         }
 
         $feedObj = Zend_Feed::importArray($rss, 'rss');
